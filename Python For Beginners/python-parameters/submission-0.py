3a3367873b70f3f70e4a46111fab109b9fc71d35
gd192509 <90369386+gauricollab09@users.noreply.github.com>
2026-07-24 00:58:34 +0800

    Add: python-parameters - submission-0

---
+++ Python For Beginners/python-parameters/submission-0.py
@@ -0,0 +1,12 @@
+def greet(name):
+    msg = "Hello, " + name
+    print(msg)
+
+def farewell(name):
+    msg = "Goodbye, " + name
+    print(msg)
+
+# don't modify below this line
+farewell("Bob")
+farewell("Charlie")
+farewell("NeetCode")
