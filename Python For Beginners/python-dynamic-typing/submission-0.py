fe93e785b485f253962496114386119f5591db19
gd192509 <90369386+gauricollab09@users.noreply.github.com>
2026-07-24 00:09:29 +0800

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,13 @@
+
+variable = 10         # int type
+print(type(variable))
+variable = 10.0        
+print(type(variable))
+variable = True       # int type
+print(type(variable))
+
+
+variable = "Hello"    # str type
+print(type(variable))
+variable = [1, 2, 3]  # list type
+print(type(variable))
